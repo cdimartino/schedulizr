e4559901b9ceba3251af9e1ea6cc4deb0e9d8940
Chris DiMartino <vwchris@gmail.com>
2015-11-18 16:38:06 -0500

    Adding basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+s1 = Schedule.create(schedule_date: Date.today)
+[
+  {
+    start_time: '9:00',
+    end_time:   '10:00',
+    name:       'Lecture'
+  },
+  {
+    start_time: '12:30',
+    end_time:   '14:00',
+    name:       'Lunch'
+  },
+  {
+    start_time: '14:00',
+    name:       'Announcements'
+  },
+  {
+    start_time: '17:30',
+    end_time:   '18:00',
+    name:       'End of Day'
+  },
+  {
+    start_time: '18:00',
+    name:       'Go Home'
+  },
+].each do |a|
+  s1.activities.create(a)
+end
