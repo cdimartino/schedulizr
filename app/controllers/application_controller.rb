38eaa3a526ee25b0d8bfb211ca2f650128166cfa
Chris DiMartino <vwchris@gmail.com>
2016-01-06 10:26:36 -0500

    Remove X-Frame-Options response header to allow iframe embed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_action :allow_iframe
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 end
