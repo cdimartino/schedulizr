6929df8cf151ab8e602ac1d461b5bc17618a6433
Chris DiMartino <vwchris@gmail.com>
2016-01-05 21:55:14 -0500

    Adding route to lookup by date

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get '/today', to: 'schedules#today'
 
+  get '/:date', to: 'schedules#by_date', constraints: { date: /\d{4}-\d{2}-\d{2}/ }
+  get '/schedules/:date', to: 'schedules#by_date', constraints: { date: /\d{4}-\d{2}-\d{2}/ }
+
   resources :schedules do
     member do
       patch 'clone'
