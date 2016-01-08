3698e6337e6da2fb4470d30f3b43e22fdc6d5ae7
Chris DiMartino <vwchris@gmail.com>
2016-01-08 11:18:18 -0500

    Make end time optional

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ActiveRecord::Base
   belongs_to :schedule
 
-  validates :start_time, :end_time, :name, presence: true
+  validates :start_time, :name, presence: true
   validates :name, uniqueness: { scope: [:start_time, :end_time, :schedule] }
 end
