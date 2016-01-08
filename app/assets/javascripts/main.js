3c876e0822f315ad44cf71b3fd803e60adec6b72
Chris DiMartino <vwchris@gmail.com>
2016-01-08 16:54:38 -0500

    Rotate!

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,15 @@
 var last_refreshed_at;
+last_interval = 0;
 $(document).ready(function() {
   last_refreshed_at = moment().format('YYYY-MM-DD');
   if ($('.schedule').length > 0) {
     var view = new ScheduleView($('.activities')[0]);
     var controller = new Controller(view);
   }
+
+  // setInterval(function() {
+  //   javascript:document.body.style.setProperty("-webkit-transform", "rotate(" + last_interval + "deg)", null);
+  //   last_interval++;
+  //   // console.log(last_interval);
+  // }, 30)
 });
